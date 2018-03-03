c23eca317c2b2a94098361c23af81106f14d179c
mikeohl <mikeohl@live.com>
2018-03-03 02:31:04 -0800

    Added comments

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,28 @@
+﻿/* ScoreKeeper maintains the score of the game in a static
+ * variable. Can reset on a new game.
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+
+	public static int score = 0;
+	
+	private Text myText;
+
+	// Use this for initialization
+	void Start () {
+		myText = GetComponent<Text>();
+	}
+	
+    // Add points to score and update in-game text
+	public void Score (int points) {
+		score += points;
+		myText.text = "Score: " + score.ToString ();
+	}
+	
+	public static void Reset () {
+		score = 0;
+	}
+}
