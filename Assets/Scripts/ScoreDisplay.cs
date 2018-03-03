b92597cf765b68326819aad1f01d971ce4c7364e
mikeohl <mikeohl@live.com>
2018-03-03 02:31:23 -0800

    Added comments

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,16 @@
+﻿/* ScoreDisplay displays score and then resets the score.
+ * For use at end of game. 
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Text myText = GetComponent<Text>();
+		myText.text = ScoreKeeper.score.ToString();
+		ScoreKeeper.Reset ();
+	}
+}
